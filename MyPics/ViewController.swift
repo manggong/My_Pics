32f0fa827821d41a84d6234fc2050dec77aac7f4
manggong <jihwank1995@gmail.com>
2021-01-01 12:37:33 +0900

    add image

--- MyPics/ViewController.swift
+++ MyPics/ViewController.swift
@@ -8,19 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currntValue = 0
 
+    @IBOutlet weak var priceLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        refresh()
     }
     
     @IBAction func showAlert(_ sender: Any) {
+        let message = "가격은 ₩\(currntValue) 입니다."
         
-        let alert = UIAlertController(title: "Hello", message: "My name is YangSSo", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Hello", message: message, preferredStyle: .alert)
         
-        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { action in self.refresh()})
         
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func refresh() {
+        let randomPrice = arc4random_uniform(10000) + 1
+        currntValue = Int(randomPrice)
+        priceLable.text = "₩\(currntValue)"
     }
 }
 
